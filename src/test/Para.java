31a6f9a2a2dcc675abbf8be0fca83e0201e3e917
Rajat1892 <36040255+Rajat1892@users.noreply.github.com>
2018-04-12 15:21:50 +0530

    new comment added

--- src/test/Para.java
+++ src/test/Para.java
@@ -12,10 +12,12 @@ public static void main(String[] args) {
 		// TODO Auto-generated method stub
 Para oo=new Para();
 Para oo1=new Para();
-String name=oo.abc("guneet", "Ritika");
+String name=oo.abc("Guneet", "Ritika");
 String name2=oo1.abc("Ritika", "sds");
 System.out.println(name);
 System.out.println(name2);
+//System.out.println(name2);
+	
 
 	}
 
